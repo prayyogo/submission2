ee05d42c263a14c3496d18aacc607055a6197dd3
prayyogo <prayyogo@gmail.com>
2019-05-20 01:01:25 +0700

    up

--- analisa.php
+++ analisa.php
@@ -76,8 +76,13 @@ if (isset($_POST['submit'])) {
 <img id="sourceImage" width="400" />
 <br>
 <h3 id="description">Memproses . . .</h3>
-<form action="https://analisagambar.azurewebsites.net/" method="post">
+<form action="index.php" method="post">
 <input type="submit" name="submit" value="Kembali">
 </form>
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
+    <script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
